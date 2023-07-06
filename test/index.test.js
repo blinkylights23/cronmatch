a3fe6ff0d2581c35a02d3a0905b6b1e3938b2ce9
Paul Smith <paul@blinkylights.org>
2023-07-06 17:38:27 +0200

    Linting

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,18 @@
-import cronmatch from '../src'
-import getRange from '../src/getRange'
+import cronmatch from '../src/index.js'
+import getRange from '../src/getRange.js'
 
 jest.useFakeTimers()
 
 describe('The getRange function', () => {
   it('should produce the correct range', () => {
-    let testRange = [1, 10]
-    let result = getRange(testRange[0], testRange[1])
+    const testRange = [1, 10]
+    const result = getRange(testRange[0], testRange[1])
     expect(result).toHaveLength(10)
     expect(result).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
   })
   it('should tolerate out-of-order values', () => {
-    let testRange = [15, 10]
-    let result = getRange(testRange[0], testRange[1])
+    const testRange = [15, 10]
+    const result = getRange(testRange[0], testRange[1])
     expect(result).toHaveLength(6)
     expect(result).toStrictEqual([10, 11, 12, 13, 14, 15])
   })
@@ -20,68 +20,69 @@ describe('The getRange function', () => {
 
 describe('The date argument', () => {
   it('should accept a Date object', () => {
-    let cronExpr = '* * * * *'
-    let date = new Date('January 20, 2020 12:00:00')
+    const cronExpr = '* * * * *'
+    const date = new Date('January 20, 2020 12:00:00')
     expect(cronmatch(cronExpr, date)).toBe(true)
   })
   it('should accept a valid date string', () => {
-    let cronExpr = '* * * * *'
-    let date = 'January 20, 2020 12:00:00'
+    const cronExpr = '* * * * *'
+    const date = 'January 20, 2020 12:00:00'
     expect(cronmatch(cronExpr, date)).toBe(true)
   })
   it('should use current time when dateArg is missing', () => {
-    var date1 = new Date('January 20, 2020 12:00:00')
-    var date2 = new Date('January 20, 2020 12:01:00')
-    let cronExpr = '0 * * * *'
+    const date1 = new Date('January 20, 2020 12:00:00')
+    const date2 = new Date('January 20, 2020 12:01:00')
+    const cronExpr = '0 * * * *'
     jest.setSystemTime(date1)
     expect(cronmatch(cronExpr)).toBe(true)
     jest.setSystemTime(date2)
     expect(cronmatch(cronExpr)).toBe(false)
   })
-  it('should return false when dateArg is invalid', () => {
-    let cronExpr = '* * * * *'
-    let date = 'January 40, 202 27:00:00'
-    expect(cronmatch(cronExpr, date)).toBe(false)
+  it('should throw when dateArg is invalid', () => {
+    const cronExpr = '* * * * *'
+    const date = 'January 40, 202 27:00:00'
+    const invoke = () => cronmatch(cronExpr, date)
+    expect(invoke).toThrow()
   })
 })
 
 describe("The cron expression parser's", () => {
   describe('minutes field', () => {
     it('should correctly parse * * * * *', () => {
-      let cronExpr = '* * * * *'
-      let date1 = new Date('January 20, 2020 12:00:00')
-      let date2 = new Date('January 20, 2020 12:01:00')
-      let date3 = new Date('January 20, 2020 12:13:00')
+      const cronExpr = '* * * * *'
+      const date1 = new Date('January 20, 2020 12:00:00')
+      const date2 = new Date('January 20, 2020 12:01:00')
+      const date3 = new Date('January 20, 2020 12:13:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(true)
     })
     it('should correctly use a step of 1 for */0 * * * *', () => {
-      let cronExpr = '*/0 * * * *'
-      let date1 = new Date('January 20, 2020 12:00:00')
-      let date2 = new Date('January 20, 2020 12:01:00')
-      let date3 = new Date('January 20, 2020 12:13:00')
+      const cronExpr = '*/0 * * * *'
+      const date1 = new Date('January 20, 2020 12:00:00')
+      const date2 = new Date('January 20, 2020 12:01:00')
+      const date3 = new Date('January 20, 2020 12:13:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(true)
     })
     it('should correctly parse 20-29 * * * *', () => {
-      let cronExpr = '20-29 * * * *'
-      let date1 = new Date('January 20, 2020 12:01:00')
-      let date2 = new Date('January 20, 2020 12:22:00')
-      let date3 = new Date('January 20, 2020 12:31:00')
+      const cronExpr = '20-29 * * * *'
+      const date1 = new Date('January 20, 2020 12:01:00')
+      const date2 = new Date('January 20, 2020 12:22:00')
+      const date3 = new Date('January 20, 2020 12:31:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(false)
     })
     it('should correctly parse 30-40/2 * * * *', () => {
-      let cronExpr = '30-40/2 * * * *'
-      let date1 = new Date('January 20, 2020 12:01:00')
-      let date2 = new Date('January 20, 2020 12:30:00')
-      let date3 = new Date('January 20, 2020 12:31:00')
-      let date4 = new Date('January 20, 2020 12:32:00')
-      let date5 = new Date('January 20, 2020 12:40:00')
-      let date6 = new Date('January 20, 2020 12:50:00')
+      const cronExpr = '30-40/2 * * * *'
+      const date1 = new Date('January 20, 2020 12:01:00')
+      const date2 = new Date('January 20, 2020 12:30:00')
+      const date3 = new Date('January 20, 2020 12:31:00')
+      const date4 = new Date('January 20, 2020 12:32:00')
+      const date5 = new Date('January 20, 2020 12:40:00')
+      const date6 = new Date('January 20, 2020 12:50:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(false)
@@ -90,14 +91,14 @@ describe("The cron expression parser's", () => {
       expect(cronmatch(cronExpr, date6)).toBe(false)
     })
     it('should correctly parse */3 * * * *', () => {
-      let cronExpr = '*/3 * * * *'
-      let date1 = new Date('January 20, 2020 12:00:00')
-      let date2 = new Date('January 20, 2020 12:01:00')
-      let date3 = new Date('January 20, 2020 12:02:00')
-      let date4 = new Date('January 20, 2020 12:03:00')
-      let date5 = new Date('January 20, 2020 12:04:00')
-      let date6 = new Date('January 20, 2020 12:05:00')
-      let date7 = new Date('January 20, 2020 12:06:00')
+      const cronExpr = '*/3 * * * *'
+      const date1 = new Date('January 20, 2020 12:00:00')
+      const date2 = new Date('January 20, 2020 12:01:00')
+      const date3 = new Date('January 20, 2020 12:02:00')
+      const date4 = new Date('January 20, 2020 12:03:00')
+      const date5 = new Date('January 20, 2020 12:04:00')
+      const date6 = new Date('January 20, 2020 12:05:00')
+      const date7 = new Date('January 20, 2020 12:06:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(false)
       expect(cronmatch(cronExpr, date3)).toBe(false)
@@ -107,14 +108,14 @@ describe("The cron expression parser's", () => {
       expect(cronmatch(cronExpr, date7)).toBe(true)
     })
     it('should correctly parse 0,13,29,51 * * * *', () => {
-      let cronExpr = '0,13,29,51 * * * *'
-      let date1 = new Date('January 20, 2020 12:00:00')
-      let date2 = new Date('January 20, 2020 12:01:00')
-      let date3 = new Date('January 20, 2020 12:13:00')
-      let date4 = new Date('January 20, 2020 12:29:00')
-      let date5 = new Date('January 20, 2020 12:51:00')
-      let date6 = new Date('January 20, 2020 12:05:00')
-      let date7 = new Date('January 20, 2020 12:55:00')
+      const cronExpr = '0,13,29,51 * * * *'
+      const date1 = new Date('January 20, 2020 12:00:00')
+      const date2 = new Date('January 20, 2020 12:01:00')
+      const date3 = new Date('January 20, 2020 12:13:00')
+      const date4 = new Date('January 20, 2020 12:29:00')
+      const date5 = new Date('January 20, 2020 12:51:00')
+      const date6 = new Date('January 20, 2020 12:05:00')
+      const date7 = new Date('January 20, 2020 12:55:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(false)
       expect(cronmatch(cronExpr, date3)).toBe(true)
@@ -124,14 +125,14 @@ describe("The cron expression parser's", () => {
       expect(cronmatch(cronExpr, date7)).toBe(false)
     })
     it('should correctly parse 20-30,48 * * * *', () => {
-      let cronExpr = '20-30,48 * * * *'
-      let date1 = new Date('January 20, 2020 12:01:00')
-      let date2 = new Date('January 20, 2020 12:20:00')
-      let date3 = new Date('January 20, 2020 12:29:00')
-      let date4 = new Date('January 20, 2020 12:30:00')
-      let date5 = new Date('January 20, 2020 12:47:00')
-      let date6 = new Date('January 20, 2020 12:48:00')
-      let date7 = new Date('January 20, 2020 12:49:00')
+      const cronExpr = '20-30,48 * * * *'
+      const date1 = new Date('January 20, 2020 12:01:00')
+      const date2 = new Date('January 20, 2020 12:20:00')
+      const date3 = new Date('January 20, 2020 12:29:00')
+      const date4 = new Date('January 20, 2020 12:30:00')
+      const date5 = new Date('January 20, 2020 12:47:00')
+      const date6 = new Date('January 20, 2020 12:48:00')
+      const date7 = new Date('January 20, 2020 12:49:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(true)
@@ -143,40 +144,40 @@ describe("The cron expression parser's", () => {
   })
   describe('hours field', () => {
     it('should correctly parse * * * * *', () => {
-      let cronExpr = '* * * * *'
-      let date1 = new Date('January 20, 2020 02:00:00')
-      let date2 = new Date('January 20, 2020 12:00:00')
-      let date3 = new Date('January 20, 2020 18:00:00')
+      const cronExpr = '* * * * *'
+      const date1 = new Date('January 20, 2020 02:00:00')
+      const date2 = new Date('January 20, 2020 12:00:00')
+      const date3 = new Date('January 20, 2020 18:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(true)
     })
     it('should correctly use a step of 1 for * */0 * * *', () => {
-      let cronExpr = '* */0 * * *'
-      let date1 = new Date('January 20, 2020 02:00:00')
-      let date2 = new Date('January 20, 2020 12:00:00')
-      let date3 = new Date('January 20, 2020 18:00:00')
+      const cronExpr = '* */0 * * *'
+      const date1 = new Date('January 20, 2020 02:00:00')
+      const date2 = new Date('January 20, 2020 12:00:00')
+      const date3 = new Date('January 20, 2020 18:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(true)
     })
     it('should correctly parse * 3-14 * * * *', () => {
-      let cronExpr = '* 3-14 * * * *'
-      let date1 = new Date('January 20, 2020 02:00:00')
-      let date2 = new Date('January 20, 2020 12:00:00')
-      let date3 = new Date('January 20, 2020 18:00:00')
+      const cronExpr = '* 3-14 * * * *'
+      const date1 = new Date('January 20, 2020 02:00:00')
+      const date2 = new Date('January 20, 2020 12:00:00')
+      const date3 = new Date('January 20, 2020 18:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(false)
     })
     it('should correctly parse * 03-15/2 * * * *', () => {
-      let cronExpr = '* 03-15/2 * * * *'
-      let date1 = new Date('January 20, 2020 01:00:00')
-      let date2 = new Date('January 20, 2020 03:00:00')
-      let date3 = new Date('January 20, 2020 04:00:00')
-      let date4 = new Date('January 20, 2020 12:00:00')
-      let date5 = new Date('January 20, 2020 13:00:00')
-      let date6 = new Date('January 20, 2020 16:00:00')
+      const cronExpr = '* 03-15/2 * * * *'
+      const date1 = new Date('January 20, 2020 01:00:00')
+      const date2 = new Date('January 20, 2020 03:00:00')
+      const date3 = new Date('January 20, 2020 04:00:00')
+      const date4 = new Date('January 20, 2020 12:00:00')
+      const date5 = new Date('January 20, 2020 13:00:00')
+      const date6 = new Date('January 20, 2020 16:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(false)
@@ -185,14 +186,14 @@ describe("The cron expression parser's", () => {
       expect(cronmatch(cronExpr, date6)).toBe(false)
     })
     it('should correctly parse * */3 * * *', () => {
-      let cronExpr = '* */3 * * *'
-      let date1 = new Date('January 20, 2020 11:00:00')
-      let date2 = new Date('January 20, 2020 12:00:00')
-      let date3 = new Date('January 20, 2020 13:00:00')
-      let date4 = new Date('January 20, 2020 14:00:00')
-      let date5 = new Date('January 20, 2020 15:00:00')
-      let date6 = new Date('January 20, 2020 16:00:00')
-      let date7 = new Date('January 20, 2020 17:00:00')
+      const cronExpr = '* */3 * * *'
+      const date1 = new Date('January 20, 2020 11:00:00')
+      const date2 = new Date('January 20, 2020 12:00:00')
+      const date3 = new Date('January 20, 2020 13:00:00')
+      const date4 = new Date('January 20, 2020 14:00:00')
+      const date5 = new Date('January 20, 2020 15:00:00')
+      const date6 = new Date('January 20, 2020 16:00:00')
+      const date7 = new Date('January 20, 2020 17:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(false)
@@ -202,12 +203,12 @@ describe("The cron expression parser's", () => {
       expect(cronmatch(cronExpr, date7)).toBe(false)
     })
     it('should correctly parse * 0,3,18 * * *', () => {
-      let cronExpr = '* 0,3,18 * * *'
-      let date1 = new Date('January 20, 2020 00:00:00')
-      let date2 = new Date('January 20, 2020 02:00:00')
-      let date3 = new Date('January 20, 2020 03:00:00')
-      let date4 = new Date('January 20, 2020 18:00:00')
-      let date5 = new Date('January 20, 2020 22:00:00')
+      const cronExpr = '* 0,3,18 * * *'
+      const date1 = new Date('January 20, 2020 00:00:00')
+      const date2 = new Date('January 20, 2020 02:00:00')
+      const date3 = new Date('January 20, 2020 03:00:00')
+      const date4 = new Date('January 20, 2020 18:00:00')
+      const date5 = new Date('January 20, 2020 22:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(true)
       expect(cronmatch(cronExpr, date2)).toBe(false)
       expect(cronmatch(cronExpr, date3)).toBe(true)
@@ -215,12 +216,12 @@ describe("The cron expression parser's", () => {
       expect(cronmatch(cronExpr, date5)).toBe(false)
     })
     it('should correctly parse * 2-3,18 * * *', () => {
-      let cronExpr = '* 2-3,18 * * *'
-      let date1 = new Date('January 20, 2020 00:00:00')
-      let date2 = new Date('January 20, 2020 02:00:00')
-      let date3 = new Date('January 20, 2020 03:00:00')
-      let date4 = new Date('January 20, 2020 18:00:00')
-      let date5 = new Date('January 20, 2020 22:00:00')
+      const cronExpr = '* 2-3,18 * * *'
+      const date1 = new Date('January 20, 2020 00:00:00')
+      const date2 = new Date('January 20, 2020 02:00:00')
+      const date3 = new Date('January 20, 2020 03:00:00')
+      const date4 = new Date('January 20, 2020 18:00:00')
+      const date5 = new Date('January 20, 2020 22:00:00')
       expect(cronmatch(cronExpr, date1)).toBe(false)
       expect(cronmatch(cronExpr, date2)).toBe(true)
       expect(cronmatch(cronExpr, date3)).toBe(true)
